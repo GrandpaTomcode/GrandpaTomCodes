f14f1603d0f814045bbaf6628a6c076e4af7e712
Tom Hornbuckle <tomhornbuckle1@gmail.com>
2024-01-02 00:24:22 +0000

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './styles/styles.sass'
+import NavBar from "./Components/Navbar.jsx"
+import HeaderSection from "./Components/HeaderSection.jsx"
+function App() {
+
+  return (
+    <>
+    <NavBar />
+    <HeaderSection/>
+    </>
+  )
+}
+
+export default App
